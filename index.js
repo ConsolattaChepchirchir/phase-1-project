d1147e5ea22a1371f730308af59e952159dba859
ConsolattaChepchirchir <consochep97@gmail.com>
2023-04-07 12:21:11 +0300

    phase-1-project

---
+++ index.js
@@ -0,0 +1,41 @@
+let  result = document.getElementById('result')
+let filter = document.getElementById('filter')
+const listItems = []
+
+getdata()
+
+filter.addEventListener('input', (e) => filterData(e.target.value))
+
+async function getdata(){
+    const res = await fetch('https://randomuser.me/api?results=20')
+    const { results } =await res.json()
+
+    result.innerHTML = ''
+
+    results.forEach(user =>{
+        let li = document.createElement('li')
+        listItems.push(li)
+        li.innerHTML =`<img src="${user.picture.large}" alt="${user.name.first}"
+        <div class = "user-info">
+
+        <h4>${user.name.first} ${user.name.last}</h4>
+        <p> ${user.location.city},${user.location.country}</p>
+
+        </div>
+    
+        `
+        result.appendChild(li)
+    })
+
+
+}
+function filterData(searchTerm){
+    listItems.forEach(item =>{
+        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())){
+            item.classList.remove('hide')
+        }
+        else{
+            item.classList.add('hide')
+        }
+    })
+}
